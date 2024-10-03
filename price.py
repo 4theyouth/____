69a852a065eb499d25c3787dfd9b2c8c0c324611
우빈 <102154880+Ubinquitous@users.noreply.github.com>
2024-10-03 17:50:31 +0900

    Add files via upload

--- price.py
+++ price.py
@@ -1,49 +1,61 @@
+import threading
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import re
 import time
 import sheet
 
+def quit_driver(driver):
+    print("Timeout reached. Quitting driver.")
+    driver.quit()
+
 def get(href):
     driver = webdriver.Chrome()
     driver.get(href)
 
-    time.sleep(0.5)
+    timer = threading.Timer(20, quit_driver, [driver])
+    timer.start()
 
-    selector = driver.find_elements(By.CSS_SELECTOR, "a.bd_1fhc9")
+    try:
+        time.sleep(0.5)
 
-    for i in selector: print(i.text)
-    selector[0].click()
+        selector = driver.find_elements(By.CSS_SELECTOR, "a.bd_1fhc9")
 
-    priceList = driver.find_elements(By.CSS_SELECTOR, "span._1LY7DqCnwR")
-    price = priceList[len(priceList) - 1].text.replace(',', '')
-    selector_list = driver.find_elements(By.CSS_SELECTOR, "li.bd_1y1pd")
-    print(selector_list)
-    print(price)
-    
-    for option in list(range(len(selector_list))):
-        time.sleep(0.5)
-        war = driver.find_elements(By.CSS_SELECTOR, "li.bd_1y1pd")
-        time.sleep(0.5)
-        warName = war[option].text
-        war[option].click()
-        time.sleep(0.5)
-        selector[1].click()
-        ops = driver.find_elements(By.CSS_SELECTOR, "a.bd_3iRne")
-        print(warName)
-        time.sleep(0.5)
-        for dti in ops:
-            price_pattern = r"\(([\+\-\d,]+)원\)"
-            match = re.search(price_pattern, dti.text)
-            name = warName + "/" + dti.text.replace(price_pattern, "")
-            try:
-                gamga = match.group(0).replace('(', '').replace(')', '').replace('원', '').replace(',', '')
-                print(int(gamga))
-                print(int(price))
-                sheet.add_column([href, name, int(gamga) + int(price)])
-            except:
-                sheet.add_column([href, name, int(price)])
-        time.sleep(0.5)
+        for i in selector: 
+            print(i.text)
         selector[0].click()
-    time.sleep(0.5)
-    driver.quit()
+
+        priceList = driver.find_elements(By.CSS_SELECTOR, "span._1LY7DqCnwR")
+        price = priceList[len(priceList) - 1].text.replace(',', '')
+        selector_list = driver.find_elements(By.CSS_SELECTOR, "li.bd_1y1pd")
+
+        for option in list(range(len(selector_list))):
+            time.sleep(0.5)
+            war = driver.find_elements(By.CSS_SELECTOR, "li.bd_1y1pd")
+            time.sleep(0.5)
+            warName = war[option].text
+            war[option].click()
+            time.sleep(0.5)
+            selector[1].click()
+            ops = driver.find_elements(By.CSS_SELECTOR, "a.bd_3iRne")
+            print(warName)
+            time.sleep(0.5)
+            for dti in ops:
+                price_pattern = r"\(([\+\-\d,]+)원\)"
+                match = re.search(price_pattern, dti.text)
+                name = warName + "/" + dti.text.replace(price_pattern, "")
+                try:
+                    gamga = match.group(0).replace('(', '').replace(')', '').replace('원', '').replace(',', '')
+                    print(int(gamga))
+                    print(int(price))
+                    sheet.add_column([href, name, int(gamga) + int(price)])
+                except:
+                    sheet.add_column([href, name, int(price)])
+            time.sleep(0.5)
+            selector[0].click()
+
+    except Exception as e:
+        print(f"An error occurred: {e}")
+    finally:
+        timer.cancel()
+        driver.quit()
